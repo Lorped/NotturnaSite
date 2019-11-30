3b9b04222ce3a505529e25e38d4bc3af2f50647c
Lorped <lorped@gmail.com>
2019-11-30 18:15:27 +0100

    pf extra

--- scheda.php
+++ scheda.php
@@ -78,12 +78,28 @@
 	}
 	$pf = (3+$attutimento)*2 + $robustezza + $schivare ;
 
+	$Mysql="SELECT * from poteri where idpotere=70 and idutente=$idutente";
+	$Result=mysql_query($Mysql);
+	if ( $res=mysql_fetch_array($Result)) {
+		$pf = $pf + $robustezza + 5 ;
+	}
+	$Mysql="SELECT * from poteri where idpotere=74 and idutente=$idutente";
+	$Result=mysql_query($Mysql);
+	if ( $res=mysql_fetch_array($Result)) {
+		$pf = $pf + 5 ;   //Resilienza già +5
+	}
 
 	// ferita permanente -3 PF
 	$Mysql="SELECT * from pregidifetti where idpregio =11 and idutente=$idutente";
 	$Result=mysql_query($Mysql);
 	if ( $res=mysql_fetch_array($Result)) {
 		$pf=$pf-3;
+	}
+	// 9 vite +10 PF
+	$Mysql="SELECT * from pregidifetti where idpregio =126 and idutente=$idutente";
+	$Result=mysql_query($Mysql);
+	if ( $res=mysql_fetch_array($Result)) {
+		$pf=$pf+10;
 	}
 
 
